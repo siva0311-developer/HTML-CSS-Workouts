ef201d23e6dd57878a51bbc04b9d09d04a4416cd
siva0311-developer <sivaram03112002@gmail.com>
2024-08-21 19:32:14 +0530

    Add files via upload

---
+++ localstorage.js
@@ -0,0 +1,10 @@
+// localStorage.setItem("name", "ram");
+// console.log(localStorage.setItem("name"));
+
+
+// document.cookie="myname=ram";
+// console.log(document.cookie);
+
+
+sessionStorage.setItem("name", "amaran");
+console.log(sessionStorage.setItem("name"));
